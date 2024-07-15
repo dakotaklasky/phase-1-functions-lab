e676489a396840eb2aa687bac9764e9f3576ab35
Dakota <dakota.klasky@gmail.com>
2024-07-15 17:07:09 -0400

    create functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x){
+    return Math.abs(x-42);
+}
+
+function distanceFromHqInFeet(x){
+    return 264*distanceFromHqInBlocks(x);
+}
+
+function distanceTravelledInFeet(x,y){
+    return Math.abs(x-y)*264;
+
+}
+
+function calculatesFarePrice(x,y){
+    if (distanceTravelledInFeet(x,y) <= 400){
+        return 0;}
+    else if (distanceTravelledInFeet(x,y) <= 2000){
+        return .02*(distanceTravelledInFeet(x,y)-400);}
+    else if (distanceTravelledInFeet(x,y) <= 2500){
+        return 25;}
+    else{
+        return "cannot travel that far";}
+    }
